6850617838d1ff5133b4fab5fd983d894e3dc45a
Anthony Lebro <antho.devjam@gmail.com>
2021-08-03 23:58:58 +0200

    Version 1.0

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+const About = () => {
+  return (
+    <div>
+      <Navigation/>
+      <Logo/>
+      <h1>A propos</h1>
+        <br/>
+        <p>blablablablabla</p>
+    </div>
+  );
+};
+
+export default About;
